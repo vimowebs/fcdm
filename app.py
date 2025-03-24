df255e030e28497787d15b407dd1841380d6df71
vimowebs <160035218+vimowebs@users.noreply.github.com>
2025-03-24 18:59:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,4 +57,5 @@ def upload_file():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
